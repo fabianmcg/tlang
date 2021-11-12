2e636c5f6551e153999ac67957f9752171070b9a
Fabian Mora <fmora.dev@gmail.com>
2021-11-12 16:07:43 -0500

    Added struct

--- lang/Lang/db.py
+++ lang/Lang/db.py
@@ -9,7 +9,7 @@ Created on Oct Sun 31 11:09:00 2021
 from Utility.dotDict import DotDict, DotDictWrapper
 from Lexer.db import LexerDB
 from Lang.node import Node
-from Lang.type import NodeType
+from Utility.type import *
 from Lang.rule import Match, InstructionRule
 from Lang.action import TokenAction, NodeAction, InstructionAction
 from Lang.instruction import *
@@ -20,29 +20,35 @@ class LangDB:
         self.tokens = tokens
         self.nodes = DotDict()
         self.extraRules = DotDict()
+        self.types = DotDict(
+            {
+                "Auto": AutoType(),
+                "Bool": BoolType(),
+                "Int": IntType(),
+                "Float": FloatType(),
+                "Void": VoidType(),
+                "String": StringType(),
+            }
+        )
 
     def __str__(self) -> str:
         return "Nodes:\n{}".format("\n".join(list(map(str, self.nodes.values()))))
 
     __repr__ = __str__
 
-    def addNode(self, identifier, dct={}):
-        self.nodes[identifier] = Node(identifier, **dct)
+    def addNode(self, identifier):
+        self.nodes[identifier] = Node(identifier)
+        self.types[identifier] = NodeType(identifier)
+        return self.nodes[identifier]
 
     def getNodes(self, function=lambda x: x):
         return DotDictWrapper(self.nodes, function)
-
-    def getNodesTypes(self):
-        return self.getNodes(lambda x: NodeType(x))
 
     def getParseTokens(self):
         return self.tokens.getTokens(lambda x: Match(TokenAction(x)))
 
     def getParseNodes(self):
         return DotDictWrapper(self.nodes, lambda x: Match(NodeAction(x)))
-
-    def getNodesIdentifiers(self):
-        return DotDictWrapper(self.nodes, lambda x: x.identifier)
 
 
 instructionDict = DotDict(
---
+++ lang/Utility/struct.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Oct Sun 31 11:09:00 2021
+
+@author: fabian
+"""
+
+from Utility.type import Type
+from Utility.variable import Variable
+from Utility.dotDict import DotDict
+
+
+class ClassParents:
+    def __init__(self, *parents):
+        self.parents = list(parents)
+
+    def __str__(self) -> str:
+        return "[" + ", ".join(list(map(str, self.parents))) + "]"
+
+    __repr__ = __str__
+
+    def __ilshift__(self, x):
+        self.parents.extend(x.parents)
+        return self
+
+    def __iadd__(self, x):
+        self.parents.append(x)
+        return self
+
+    def empty(self):
+        return len(self.parents) == 0
+
+
+class ClassMembers(DotDict):
+    def __init__(self, *members):
+        super().__init__({m.identifier: m for m in members})
+
+    def __ilshift__(self, x):
+        self.update(x)
+        return self
+
+    def __iadd__(self, x):
+        self[x.identifier] = x
+        return self
+
+    def __str__(self) -> str:
+        return str(list(self.values()))
+
+    __repr__ = __str__
+
+    def empty(self):
+        return len(self) == 0
+
+
+class ClassMethods(DotDict):
+    def __init__(self, *methods):
+        super().__init__({m.identifier: m for m in methods})
+
+    def __ilshift__(self, x):
+        self.update(x)
+        return self
+
+    def __iadd__(self, x):
+        self[x.identifier] = x
+        return self
+
+    def __str__(self) -> str:
+        return str(list(self.values()))
+
+    __repr__ = __str__
+
+    def empty(self):
+        return len(self) == 0
+
+
+class Class(Type):
+    def __init__(
+        self,
+        typename,
+        parents=None,
+        members=None,
+        methods=None,
+    ):
+        super().__init__(typename)
+        self.parents = parents or ClassParents()
+        self.members = members or ClassMembers()
+        self.methods = methods or ClassMethods()
+
+    def __str__(self) -> str:
+        return "{} {{\n  Parents: {}\n  Members: {}\n  Methods: {}\n}};".format(
+            self.T, self.parents, self.members, self.methods
+        )
+
+    __repr__ = __str__
+
+    def __getattr__(self, k):
+        if k in self.members:
+            return self.members[k]
+        elif k in self.methods:
+            return self.methods[k]
+        raise (KeyError("Unknown key: {}".format(k)))
+
+    def __iadd__(self, x):
+        if isinstance(x, Type):
+            self.parents += x
+        elif isinstance(x, Variable):
+            self.members += x
+        return self
+
+    def __ilshift__(self, x):
+        if isinstance(x, ClassParents):
+            self.parents <<= x
+        elif isinstance(x, ClassMembers):
+            self.members <<= x
+        elif isinstance(x, ClassMethods):
+            self.methods <<= x
+        return self
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        pass
