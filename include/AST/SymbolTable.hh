2f09ed7f241d6e60c3b7ad33029ace61d2134e7c
Fabian Mora <fmora.dev@gmail.com>
2022-02-27 19:41:50 -0500

    Splitted the grammar

--- include/AST/SymbolTable.hh
+++ include/AST/SymbolTable.hh
@@ -6,23 +6,27 @@
 #include "Decl.hh"
 #include "ASTNode.hh"
 #include "TypeContext.hh"
+#include <list>
 #include <map>
 
 namespace _astnp_ {
 
 struct SymbolTable {
-  std::map<std::string, Decl*> context { };
+  std::map<std::string, std::list<Decl*>> context { };
   SymbolTable *parent { };
   SymbolTable() = default;
   ~SymbolTable() = default;
   SymbolTable(SymbolTable *p) :
       parent(p) {
   }
-  void add(const std::string&, ASTNode *node) {
-
+  void add(const std::string& id, Decl *node) {
+    if (node)
+      context[id].push_back(node);
   }
-  void remove(const std::string&) {
-
+  void remove(const std::string& id) {
+    auto it = context.find(id);
+    if (it != context.end())
+      context.erase(id);
   }
   Decl* find(const std::string&, bool local) {
     return nullptr;
