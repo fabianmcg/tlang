77e90484005a7e5f6cd679a1888f6a37083051f8
fmorac <fabian.mcg@crpl.udel.edu>
2022-03-30 21:36:20 -0400

    TC?

--- include/AST/ASTContext.hh
+++ include/AST/ASTContext.hh
@@ -1,8 +1,8 @@
-#ifndef __AST_ASTCONTEXT_HH__
-#define __AST_ASTCONTEXT_HH__
+#ifndef AST_ASTCONTEXT_HH
+#define AST_ASTCONTEXT_HH
 
 #include "Common.hh"
-//#include "TypeContext.hh"
+#include "TypeContext.hh"
 #include <Analysis/AnalysisContext.hh>
 #include <cstdint>
 #include <map>
@@ -13,21 +13,21 @@ struct ASTContext {
   ASTContext() {
   }
   ~ASTContext() {
-    __module == nullptr;
+    module == nullptr;
   }
   ASTContext(ASTContext&&) = default;
   ASTContext(const ASTContext&) = delete;
   ASTContext& operator=(ASTContext&&) = default;
   ASTContext& operator=(const ASTContext&) = delete;
   ModuleDecl* operator*() const {
-    return __module;
+    return module;
   }
   template <typename T, typename ...Args>
   T* make(Args &&...args) {
     return add_node(std::make_unique<T>(std::forward<Args>(args)...));
   }
   template <typename T, typename V>
-  T* create(V&& val) {
+  T* create(V &&val) {
     return add_node(std::make_unique<V>(std::forward<V>(val)));
   }
   template <typename T>
@@ -36,27 +36,31 @@ struct ASTContext {
   }
   template <typename T>
   void remove(T *node) {
-    auto it = __nodes.find(static_cast<uint64_t>(node));
-    if (it != __nodes.end())
-      __nodes.erase(it);
+    auto it = nodes.find(static_cast<uint64_t>(node));
+    if (it != nodes.end())
+      nodes.erase(it);
   }
   void add_module(ModuleDecl *module) {
-    __module = module;
+    this->module = module;
   }
   AnalysisContext& analysisContext() {
-    return __analyis;
+    return analyis;
   }
   const AnalysisContext& analysisContext() const {
-    return __analyis;
+    return analyis;
+  }
+  TypeContext& types() {
+    return type_context;
   }
 protected:
-  std::map<uint64_t, std::unique_ptr<ASTNode>> __nodes;
-  AnalysisContext __analyis { };
-  ModuleDecl *__module { };
+  std::map<uint64_t, std::unique_ptr<ASTNode>> nodes;
+  AnalysisContext analyis { };
+  ModuleDecl *module { };
+  TypeContext type_context;
   template <typename T>
   T* add_node(std::unique_ptr<T> &&value) {
     if (value) {
-      auto &node = __nodes[reinterpret_cast<std::uintptr_t>(value.get())] = std::forward<std::unique_ptr<T>>(value);
+      auto &node = nodes[reinterpret_cast<std::uintptr_t>(value.get())] = std::forward<std::unique_ptr<T>>(value);
       return static_cast<T*>(node.get());
     }
     return nullptr;
--- include/AST/TypeContext.hh
+++ include/AST/TypeContext.hh
@@ -1,65 +1,121 @@
-#ifndef __AST_TYPECONTEXT_HH__
-#define __AST_TYPECONTEXT_HH__
+#ifndef AST_TYPECONTEXT_HH
+#define AST_TYPECONTEXT_HH
 
-#include "Common/Macros.hh"
-#include "ASTCommon.hh"
 #include <cstdint>
 #include <map>
+#include <memory>
+#include <set>
+#include "Decl.hh"
+#include "Type.hh"
 
 namespace tlang {
 class TypeContext {
 public:
   TypeContext() {
-    init_numeric();
+    init();
   }
-  Type* base_types(Type *type) const {
-    switch (type->classof()) {
-    case NodeClass::DependentType:
-      return rm_const(dependent_type);
-    case NodeClass::BoolType:
-      return rm_const(bool_type);
-    case NodeClass::StringType:
-      return rm_const(string_type);
-    case NodeClass::IntType: {
-      IntType *it = static_cast<IntType*>(type);
-      return rm_const(IntTypes[it->getSign()][it->getPrecision()]);
-    }
-    case NodeClass::FloatType: {
-      FloatType *it = static_cast<FloatType*>(type);
-      return rm_const(FloatTypes[it->getPrecision()]);
-    }
+  UnresolvedType* getUnresolvedType(const Identifier &id) {
+    return addType(UnresolvedType(id));
+  }
+  AddressType* getAddressType() {
+    return &addres_type;
+  }
+  BoolType* getBoolType() {
+    return &bool_type;
+  }
+  StringType* getStringType() {
+    return &string_type;
+  }
+  IntType* getIntType(IntType::numeric_precision precision, IntType::numeric_sign sign) {
+    return &(int_types[sign][precision]);
+  }
+  FloatType* getFloatType(FloatType::numeric_precision precision) {
+    return &(float_types[precision]);
+  }
+  VariadicType* getVariadicType(Type *underlying) {
+    return addType(VariadicType(underlying));
+  }
+  PtrType* getPtrType(Type *underlying) {
+    if (underlying) {
+      auto &type = ptrTypes[underlying];
+      if (type)
+        return type;
+      if (!underlying->getCanonicalType())
+        throw(std::runtime_error("Invalid underlying type"));
+      auto &canonicalType = ptrTypes[underlying->getCanonicalType().data()];
+      if (!canonicalType) {
+        canonicalType = addType(PtrType(underlying->getCanonicalType().data()));
+        canonicalType->getCanonicalType() = canonicalType;
+      }
+      type = addType(PtrType(underlying));
+      type->getCanonicalType() = canonicalType;
+      return type;
     }
     return nullptr;
   }
-  Type* static_types(Type *type) {
-    switch (type->classof()) {
-    case NodeClass::PtrType:
-      return rm_const(dependent_type);
+  ArrayType* getArrayType(Type *underlying, List<Expr*> &&layout) {
+    return addType(ArrayType(underlying, std::forward<List<Expr*>>(layout)));
+  }
+  StructType* getStructType(StructDecl *decl) {
+    if (decl) {
+      auto &type = decl_types[decl];
+      if (type)
+        return static_cast<StructType*>(type.get());
+      auto st = std::make_unique<StructType>();
+      st.get()->getDecl() = decl;
+      st->getCanonicalType() = st.get();
+      type = std::move(st);
+      return static_cast<StructType*>(type.get());
+    }
+    return nullptr;
+  }
+  EnumType* getEnumType(EnumDecl *decl) {
+    if (decl) {
+      auto &type = decl_types[decl];
+      if (type)
+        return static_cast<EnumType*>(type.get());
+      auto st = std::make_unique<EnumType>();
+      st->getDecl() = decl;
+      st->getCanonicalType() = st.get();
+      type = std::move(st);
+      return static_cast<EnumType*>(type.get());
     }
     return nullptr;
   }
 protected:
-  DependentType dependent_type { };
+  AddressType addres_type { };
   BoolType bool_type { };
   StringType string_type { };
-  std::array<std::array<IntType, 5>, 2> IntTypes { };
-  std::array<FloatType, 6> FloatTypes { };
-  void init_numeric() {
-    auto add_float = [&](FloatType::numeric_precision_t precision) {
-      FloatTypes[precision] = FloatType { precision };
+  std::array<std::array<IntType, 5>, 2> int_types { };
+  std::array<FloatType, 6> float_types { };
+  std::map<Decl*, std::unique_ptr<Type>> decl_types;
+  std::map<Type*, std::unique_ptr<Type>> types;
+  std::map<Type*, PtrType*> ptrTypes;
+  template <typename T>
+  T* addType(T &&type) {
+    auto t = std::make_unique<T>(std::move(type));
+    auto address = t.get();
+    types[address] = std::move(t);
+    address->getCanonicalType() = address;
+    return address;
+  }
+  void init() {
+    addres_type.getCanonicalType() = &addres_type;
+    bool_type.getCanonicalType() = &bool_type;
+    string_type.getCanonicalType() = &string_type;
+    auto add_float = [&](FloatType::numeric_precision precision) {
+      float_types[precision] = FloatType { precision };
+      float_types[precision].getCanonicalType() = &(float_types[precision]);
     };
-    auto add_int = [&](IntType::numeric_precision_t precision, IntType::numeric_sign_t sign) {
-      IntTypes[sign][precision] = IntType { precision, sign };
+    auto add_int = [&](IntType::numeric_precision precision, IntType::numeric_sign sign) {
+      int_types[sign][precision] = IntType { precision, sign };
+      int_types[sign][precision].getCanonicalType() = &(int_types[sign][precision]);
     };
     for (int p = FloatType::Default; p <= FloatType::P_128; ++p)
-      add_float(static_cast<FloatType::numeric_precision_t>(p));
+      add_float(static_cast<FloatType::numeric_precision>(p));
     for (int s = IntType::Signed; s <= IntType::Unsigned; ++s)
       for (int p = IntType::Default; p <= IntType::P_64; ++p)
-        add_int(static_cast<IntType::numeric_precision_t>(p), static_cast<IntType::numeric_sign_t>(s));
-  }
-  template <typename T>
-  std::remove_const_t<T>* rm_const(T &type) const {
-    return const_cast<std::remove_const_t<T>*>(&type);
+        add_int(static_cast<IntType::numeric_precision>(p), static_cast<IntType::numeric_sign>(s));
   }
 };
 }
